6c9c8a9aedcd5b17ce9286dc14d01ba1b76736a6
CyberL1 <cyber.hf18@gmail.com>
2025-06-30 18:34:14 +0200

    feat: basic frontend with container list

--- frontend/src/pages/Error.tsx
+++
@@ -1,7 +0,0 @@
-import { ErrorResponse, useRouteError } from "react-router";
-
-export default function ErrorPage() {
-	const error = useRouteError() as Error & ErrorResponse;
-
-	return <>Error: {error.statusText || error.message}</>;
-}
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let containers = [] as { name: string, status: string }[];
+
+  onMount(async () => {
+    const response = await fetch("/api/containers");
+    containers = await response.json();
+  });
+
+  async function deleteContainer(id: string) {
+    if (confirm("Are you sure you want to delete this container?")) {
+      const deleteResp = await fetch(`/api/containers/${id}?force=true`, { method: "DELETE" });
+
+      if (!deleteResp.ok) {
+        alert("Failed to delete container");
+        return;
+      }
+
+      // Refetch containers
+      const response = await fetch("/api/containers");
+      containers = await response.json();
+    }
+  }
+</script>
+
+Containers:
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Status</th>
+      <th>Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each containers as container}
+      <tr>
+        <td>{container.name}</td>
+        <td>{container.status}</td>
+        <td>
+          <button on:click={() => window.open(`//${container.name}.${location.host}`)}>Open container</button>
+        <button on:click={() => deleteContainer(container.name)}>Delete</button></td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
+<style>
+  table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+  th, td {
+    border: 1px solid #ccc;
+    padding: 0.5rem;
+    text-align: left;
+  }
+  th {
+    background: #f4f4f4;
+  }
+</style>
