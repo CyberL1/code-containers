6efc10f7122a21eb6b1b689c6dcd8e7205ad2232
CyberL1 <cyber.hf18@gmail.com>
2025-07-02 05:57:37 +0200

    style(server): format

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,9 @@ config({ path: "../.env" });
 
 import fastifyWebsocket from "@fastify/websocket";
 import fastify, {
-	type RouteOptions,
-	type HTTPMethods,
-	type RouteHandler,
+  type RouteOptions,
+  type HTTPMethods,
+  type RouteHandler,
 } from "fastify";
 import { readdirSync } from "fs";
 
@@ -13,61 +13,61 @@ const app = fastify();
 app.register(fastifyWebsocket);
 
 const routes = readdirSync(`${import.meta.dirname}/routes`, {
-	recursive: true,
+  recursive: true,
 });
 
 for (let file of routes) {
-	if (typeof file === "string") {
-		if (!file.endsWith(".ts")) {
-			continue;
-		}
+  if (typeof file === "string") {
+    if (!file.endsWith(".ts")) {
+      continue;
+    }
 
-		file = file.replaceAll("\\", "/");
+    file = file.replaceAll("\\", "/");
 
-		let route = `/${file.split(".").slice(0, -1).join(".")}`;
-		route = route.replaceAll("_", ":");
+    let route = `/${file.split(".").slice(0, -1).join(".")}`;
+    route = route.replaceAll("_", ":");
 
-		let routePath = route.endsWith("/index") ? route.slice(0, -6) : route;
-		if (routePath === "") {
-			routePath = "/";
-		}
+    let routePath = route.endsWith("/index") ? route.slice(0, -6) : route;
+    if (routePath === "") {
+      routePath = "/";
+    }
 
-		console.log(`Loading route: ${routePath}`);
+    console.log(`Loading route: ${routePath}`);
 
-		const routeModule = await import(`./routes/${file}`);
-		const ROUTE_METHODS: HTTPMethods[] = [
-			"get",
-			"post",
-			"put",
-			"delete",
-			"patch",
-		];
+    const routeModule = await import(`./routes/${file}`);
+    const ROUTE_METHODS: HTTPMethods[] = [
+      "get",
+      "post",
+      "put",
+      "delete",
+      "patch",
+    ];
 
-		Object.entries(routeModule.methods).forEach(([method, options]) => {
-			ROUTE_METHODS.splice(ROUTE_METHODS.indexOf(method), 1);
+    Object.entries(routeModule.methods).forEach(([method, options]) => {
+      ROUTE_METHODS.splice(ROUTE_METHODS.indexOf(method), 1);
 
-			if (options instanceof Function) {
-				(options as unknown as RouteOptions).handler = options as RouteHandler;
-			}
+      if (options instanceof Function) {
+        (options as unknown as RouteOptions).handler = options as RouteHandler;
+      }
 
-			app.route({
-				method,
-				url: routePath,
-				...(options as RouteOptions),
-			});
-		});
+      app.route({
+        method,
+        url: routePath,
+        ...(options as RouteOptions),
+      });
+    });
 
-		for (const method of ROUTE_METHODS) {
-			app.route({
-				method,
-				url: routePath,
-				handler: () => ({
-					code: "METHOD_NOT_ALLOWED",
-					message: `${method.toUpperCase()} is not allowed`,
-				}),
-			});
-		}
-	}
+    for (const method of ROUTE_METHODS) {
+      app.route({
+        method,
+        url: routePath,
+        handler: () => ({
+          code: "METHOD_NOT_ALLOWED",
+          message: `${method.toUpperCase()} is not allowed`,
+        }),
+      });
+    }
+  }
 }
 
 await app.listen({ port: Number(process.env.PORT), host: process.env.HOST });
